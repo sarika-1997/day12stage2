1166fb477f0057fd6e2a388acd2d12b0e3b0383f
Sarika Shardul <shardulsarika@gmail.com>
2021-11-18 18:41:51 +0530

    Day12stage2

---
+++ day12stage2.sh
@@ -0,0 +1,25 @@
+RANDOM=$$
+
+for i in `seq 10`
+do
+        echo "$(( RANDOM%1000-1 ))"
+done
+
+read echo
+small=${i[0]}
+greatest=${i[0]}
+
+for ((j=0; j<i; j++))
+do
+	if [ ${i[j]} -lt $small ];
+then
+	small=${i[$j]}
+elif
+	[ ${i[j]} -gt $greatest ];
+then
+	greatest=${i[j]}
+fi
+done
+
+echo "Smallest number in an array is: $small"
+echo "greatest number in an array is: $greatest"
